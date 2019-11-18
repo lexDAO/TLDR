5190045f7389ecf54bdfc7eeb46ba0628a34f5bc
joshma91 <josh.ma91@gmail.com>
2019-11-17 23:04:18 -0800

    add register ddr section

--- client/src/App.js
+++ client/src/App.js
@@ -18,11 +18,23 @@ export default function App() {
   const [web3, setWeb3] = useState(0);
   const [accounts, setAccounts] = useState();
   const [contract, setContract] = useState();
+
+  // Submit a LexScript Template
   const [template, setTemplate] = useState();
   const [rate, setRate] = useState();
   const [lexAddress, setLexAddress] = useState();
+
+  // Read LexScript Template
   const [lexScriptID, setlexID] = useState();
   const [lexScript, setLexScript] = useState();
+
+  // Register DDR
+  const [provider, setProvider] = useState();
+  const [DDRToken, setDDRToken] = useState();
+  const [deliverable, setDeliverable] = useState();
+  const [retainerDuration, setRetinerDuration] = useState();
+  const [deliverableRate, setDeliverableRate] = useState();
+  const [payCap, setPayCap] = useState();
 
   const fetchData = async () => {
     try {
@@ -64,6 +76,23 @@ export default function App() {
     setLexScript(lexScript.templateTerms);
   };
 
+  const registerDDR = async () => {
+    const res = await contract.methods
+      .registerDDR(
+        accounts[0],
+        provider,
+        DDRToken,
+        deliverable,
+        retainerDuration,
+        deliverableRate,
+        payCap,
+        lexScriptID
+      )
+      .send({ from: accounts[0], gas: 300000 });
+
+    console.log(res);
+  };
+
   if (!web3) {
     return <div>Loading Web3, accounts, and contract...</div>;
   }
@@ -77,8 +106,7 @@ export default function App() {
         <Header as="h2">The lexDAO Registry</Header>
 
         <Segment>
-        <Header as="h3">Submit a LexScript</Header>
-
+          <Header as="h3">Submit a LexScript</Header>
           <Form>
             <Form.Field
               control={Input}
@@ -108,7 +136,7 @@ export default function App() {
           </Form>
         </Segment>
         <Segment>
-        <Header as="h3">Read a LexScript</Header>
+          <Header as="h3">Select/Read a LexScript</Header>
           <Form>
             <Form.Field
               control={Input}
@@ -122,6 +150,56 @@ export default function App() {
             </Button>
           </Form>
           {lexScript && <Message>{lexScript}</Message>}
+        </Segment>
+        <Segment>
+          <Header as="h3">Register a DDR</Header>
+          <Form>
+            <Form.Field
+              control={Input}
+              placeholder="0x0000000000000000000000000000000000000000"
+              label="Provider Address"
+              value={provider}
+              onChange={e => setProvider(e.target.value)}
+            />
+            <Form.Field
+              control={Input}
+              placeholder="0x0000000000000000000000000000000000000000"
+              label="DDR Token Address"
+              value={DDRToken}
+              onChange={e => setDDRToken(e.target.value)}
+            />
+            <Form.Field
+              control={Input}
+              label="Deliverable Description"
+              placeholder="Description..."
+              value={deliverable}
+              onChange={e => setDeliverable(e.target.value)}
+            />
+            <Form.Field
+              control={Input}
+              placeholder="1"
+              label="Retainer Duration (days)"
+              value={retainerDuration}
+              onChange={e => setRetinerDuration(e.target.value)}
+            />
+            <Form.Field
+              control={Input}
+              placeholder="1"
+              label="Deliverable Rate"
+              value={deliverableRate}
+              onChange={e => setDeliverableRate(e.target.value)}
+            />
+            <Form.Field
+              control={Input}
+              placeholder="1 ETH"
+              label="Pay Cap (in ETH)"
+              value={payCap}
+              onChange={e => setPayCap(e.target.value)}
+            />
+            <Button type="submit" onClick={registerDDR}>
+              Submit
+            </Button>
+          </Form>
         </Segment>
       </Container>
     </div>
